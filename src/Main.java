e74287327b8ac901c4d248fb2b2e01e1aa640b41
hedgehogwars <tekountikov@gmail.com>
2025-02-08 17:16:15 -0500

    modularized the code a bit more

--- src/Main.java
+++ src/Main.java
@@ -13,48 +13,54 @@ public class Main {
         for(int i = 1;i<randomLine;i++){
             file.next();
         }
-        String word = file.next();
-        System.out.println(word+"     "+randomLine);
-        return word;
+        return file.next();
     }
     public static void hangMan(String word,int guesses){
-        boolean won = false;
+        boolean[] won = {false};//won is an array to allow methods to change it, I do not know of any better methods
         int guessesLeft;
         String[] letters = new String[26];
         String[] wordArray = word.split("");
         boolean[] lettersGuessedCorrect = new boolean[word.length()];
-        Scanner input = new Scanner(System.in);
         for(guessesLeft = guesses; guessesLeft>=0;guessesLeft--){
             String letter;
             if(guessesLeft!=guesses){
                 displayHangMan(lettersGuessedCorrect,word,wordArray,letters,guesses,guessesLeft);
             }
-            while(true){
-                System.out.println("Enter letter to guess");
-                letter = input.next().toLowerCase();
-                if(letter.equals(word)){
-                    won = true;
-                    break;
-                }
-                if(letter.length() != 1||!Character.isLetter(letter.charAt(0))){
-                    System.out.println("That is not a letter, try again");
-                }
-                if(Arrays.asList(letters).contains(letter)){
-                    System.out.println("You have already guessed this letter");
-                }
-                break;
-            }
-            if(won ==true){break;}
+            letter = getGuess(letters,guessesLeft,word,won);
+
+            if(won[0]){break;}
             for(int i = 0;i<wordArray.length;i++){
                 if(wordArray[i].equals(letter)){lettersGuessedCorrect[i] = true;}
             }
             letters[guesses-guessesLeft] = letter;
         }
-        if(won == true){
+        if(won[0]){
             System.out.println("Congratulations, you correctly guessed the word! It was "+word+". You had "+guessesLeft+" guesses left.");
         }
         else{
-            System.out.println("You lost, maybe try again?");
+            System.out.println("You lost, The word was "+word+", maybe try again?");
+        }
+    }
+
+    private static String getGuess(String[] letters,int guessesLeft, String word,boolean[] won) {
+        Scanner input = new Scanner(System.in);
+        String letter;
+        while(true){
+            System.out.println();
+            System.out.println("Enter a guess");
+            System.out.println("You have "+guessesLeft+" guesses left");
+            letter = input.next().toLowerCase();
+            if(letter.equals(word)){
+                won[0] = true;
+                break;
+            }
+            if(letter.length() != 1||!Character.isLetter(letter.charAt(0))){
+                System.out.println("That is not a letter, try again");
+            }
+            else if(Arrays.asList(letters).contains(letter)){
+                System.out.println("You have already guessed this letter");
+            }
+            else{break;}
         }
     }
 
@@ -66,18 +72,14 @@ public class Main {
         }
         System.out.println();
         System.out.println("You have guessed:");
-        for(int i = 0; i<guesses-guessesLeft-1;i++){
-            System.out.print(letters[i]+", ");
-
+        for(int i = 0; i<guesses-guessesLeft-1;i++) {
+            System.out.print(letters[i] + ", ");
         }
-        System.out.print(letters[guesses-guessesLeft-1]);
-        System.out.println();
-        System.out.println("You have "+guessesLeft+" guesses left.");
     }
 
     public static void main(String[] args) throws FileNotFoundException {
         String word = randomWord();
-        int guesses = (int)(((word.length()*2)/3)+.5);
+        int guesses = (int)(((word.length()*2)/3)+.5);// the math is for having it be comparatively fair between the large and small words
         hangMan(word,guesses);
     }
 }
